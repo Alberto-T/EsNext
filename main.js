cf410f02ea98d9bf64e3d988ed6283bcb328d0b8
Alberto-T <alberto.torres.cancino@gmail.com>
2024-04-08 18:37:10 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css';
+// import './01-arrays/array-structured-clone';
+import './01-arrays/array-with';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+    
+`
